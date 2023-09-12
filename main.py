66a37af2a5150ddc8d26b7289264049075c104d9
Faiza Syed <110279010+Faiza-syed@users.noreply.github.com>
2023-09-12 17:15:26 +0500

    Create main.py
    Simple calculator for people who are beginners in python language

---
+++ main.py
@@ -0,0 +1,36 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("Enter two numbers:")
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1-4): ")
+
+if choice == '1':
+    result = add(num1, num2)
+elif choice == '2':
+    result = subtract(num1, num2)
+elif choice == '3':
+    result = multiply(num1, num2)
+elif choice == '4':
+    result = divide(num1, num2)
+else:
+    print("Invalid choice")
+
+print("Result:", result)
